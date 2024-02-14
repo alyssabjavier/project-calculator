c7cc6da1678d9c39f6f4d73c777a40c4b7b106fe
“alyssabjavier” <alyssabjavier@gmail.com>
2024-02-13 22:12:23 -0600

    omg a functioning calculator

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const multiply = ((a,b) => a * b);
 const divide = ((a,b) => a / b);
 
 //operator function
+let operator = '';
 let operate = function(num1, num2, operator) {
     let result;
     switch(operator) {
@@ -26,6 +27,7 @@ let operate = function(num1, num2, operator) {
 
 //populating display
 let displayValue = '';
+let num1str = '';
 const displayDiv = document.getElementById('displayDiv');
 displayDiv.textContent = displayValue;
 
@@ -35,3 +37,26 @@ numBtns.forEach(btn => btn.addEventListener('click', function(event) {
     displayValue += btn.value;
     displayDiv.textContent = displayValue;
 }));
+
+let mathBtns = document.querySelectorAll('.mathBtn');
+mathBtns.forEach(btn => btn.addEventListener('click', function(event) {
+    event.preventDefault();
+    operator = btn.value;
+    num1str = displayValue;
+        console.log({operator});
+        console.log(btn.value);
+        console.log({displayValue})
+        console.log({num1str});
+     displayValue = '';
+         console.log({displayValue});
+    // displayDiv.textContent = displayValue;
+}));
+
+let equalBtn = document.getElementById('equalBtn');
+equalBtn.addEventListener('click', function() {
+    let num1 = parseFloat(num1str);
+    let num2 = parseFloat(displayValue);
+        console.log({num2});
+    let result = operate(num1, num2, operator);
+    displayDiv.textContent = result;
+})
